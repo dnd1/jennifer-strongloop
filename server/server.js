04760f6d5b0d6d59b039336ee1c24bc821eb796d
¨jennifer¨ <jennifer.zambrano@dnd1.com>
2015-09-11 14:21:13 -0430

    primera parte del curso de Strongloop

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, __dirname);
+
 app.start = function() {
   // start the web server
   return app.listen(function() {
@@ -11,12 +15,11 @@ app.start = function() {
   });
 };
 
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
-
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+app.use('/express-status', function(req, res, next) {
+  res.json({ running: true });
 });
+
+// start the server if `$ node server.js`
+if (require.main === module) {
+  app.start();
+}
