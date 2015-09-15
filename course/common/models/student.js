41d3b80d1ce094783522a127f071dafcceb4d6c5
¨jennifer¨ <jennifer.zambrano@dnd1.com>
2015-09-15 15:49:31 -0430

    primer avance de la practica

---
+++ course/common/models/student.js
@@ -0,0 +1,16 @@
+module.exports = function(Student) {
+  //
+  //Student.greet = function(msg, cb) {
+  //  cb(null, 'Greetings... ' + msg);
+  //}
+  //
+  //Student.remoteMethod(
+  //  'greet',
+  //  {
+  //    accepts: {arg: 'msg', type: 'string'},
+  //    returns: {arg: 'greeting', type: 'string'},
+  //    http:    {path: '/Students/{id}', verb: 'get'}
+  //  }
+  //);
+
+};
